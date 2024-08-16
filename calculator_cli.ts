12fc426df24f19d786ceda63e8de6486ecf25acb
sayanok <rng.jhn.osca@gmail.com>
2024-08-17 06:58:58 +0900

    modify シンプルにした

--- calculator_cli.ts
+++ calculator_cli.ts
@@ -7,13 +7,12 @@ program.name("calculator_cli").description("計算を行うCLIです");
 program
   .command("add")
   .description("加算する")
-  .option("-s, --separator <char>", "<char>で入力された数値を区切る")
   .action((str, options) => {
     let n = 0;
     let result = 0;
 
-    while (n < String(options.args).split(str.separator).length) {
-      result += Number(String(options.args).split(str.separator)[n]);
+    while (n < options.args.length) {
+      result += Number(options.args[n]);
       n++;
     }
     console.log(result);
