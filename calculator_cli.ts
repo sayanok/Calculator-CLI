cccd32786fb91bd691a20bafe75c355e15ae4c99
sayanok <rng.jhn.osca@gmail.com>
2024-08-17 07:24:25 +0900

    add floatingNumber

--- calculator_cli.ts
+++ calculator_cli.ts
@@ -7,12 +7,13 @@ program.name("calculator_cli").description("計算を行うCLIです");
 program
   .command("add")
   .description("加算する")
+  .option("-f", "floating number")
   .action((str, options) => {
     let n = 0;
     let result = 0;
 
     while (n < options.args.length) {
-      result += Number(options.args[n]);
+      str ? (result += Number.parseFloat(options.args[n])) : (result += Number(options.args[n]));
       n++;
     }
     console.log(result);
