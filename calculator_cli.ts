faa424c1dcea8b0c4755c4e9a010948b6294abe9
sayanok <rng.jhn.osca@gmail.com>
2024-08-20 16:41:40 +0900

    modify

--- calculator_cli.ts
+++ calculator_cli.ts
@@ -2,39 +2,49 @@ import { Command } from "commander";
 
 const program = new Command();
 
+function calculate(result: number, number: number) {
+  return result + number;
+}
+
+const even = program.createCommand("even").action((str, options) => {
+  let n = 0;
+  let result = 0;
+
+  while (n < options.args.length) {
+    Number(options.args[n]) % 2 === 0 ? (result = calculate(Number(result), Number(options.args[n]))) : result;
+    n++;
+  }
+  console.log(result);
+});
+
+const odd = program.createCommand("odd").action((str, options) => {
+  let n = 0;
+  let result = 0;
+
+  while (n < options.args.length) {
+    Number(options.args[n]) % 2 === 1 ? (result = calculate(Number(result), Number(options.args[n]))) : result;
+    n++;
+  }
+  console.log(result);
+});
+
 program.name("calculator_cli").description("計算を行うCLIです");
 
 program
   .command("add")
   .description("加算する")
-  .option("--even", "偶数のみ加算する")
-  .option("--odd", "偶数のみ加算する")
   .option("-f", "floating number")
+  .addCommand(even)
+  .addCommand(odd)
   .action((str, options) => {
     let n = 0;
     let result = 0;
 
     while (n < options.args.length) {
       if (str.f) {
-        if (str.even && str.odd) {
-          Number.isInteger(Number(options.args[n])) ? (result += Number(options.args[n])) : result;
-        } else if (str.even && !str.odd) {
-          Number(options.args[n]) % 2 === 0 ? (result += Number(options.args[n])) : result;
-        } else if (!str.even && str.odd) {
-          Number(options.args[n]) % 2 === 1 ? (result += Number(options.args[n])) : result;
-        } else {
-          result += Number.parseFloat(options.args[n]);
-        }
+        result = calculate(result, Number.parseFloat(options.args[n]));
       } else {
-        if (str.even && str.odd) {
-          Number.isInteger(Number(options.args[n])) ? (result += Number(options.args[n])) : result;
-        } else if (str.even && !str.odd) {
-          Number(options.args[n]) % 2 === 0 ? (result += Number(options.args[n])) : result;
-        } else if (!str.even && str.odd) {
-          Number(options.args[n]) % 2 === 1 ? (result += Number(options.args[n])) : result;
-        } else {
-          Number.isInteger(Number(options.args[n])) ? (result += Number(options.args[n])) : result;
-        }
+        Number.isInteger(Number(options.args[n])) ? (result = calculate(result, Number(options.args[n]))) : result;
       }
       n++;
     }
