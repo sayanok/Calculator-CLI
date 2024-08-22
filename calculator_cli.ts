045a36d2878556e6329efbc641f9010fd2b804eb
sayanok <rng.jhn.osca@gmail.com>
2024-08-22 16:11:45 +0900

    modify 一応動くようになった

--- calculator_cli.ts
+++ calculator_cli.ts
@@ -1,50 +1,65 @@
 import { Command } from "commander";
 
 const program = new Command();
-
-function calculate(result: number, number: number) {
-  return result + number;
-}
+program.name("calculator_cli").description("計算を行うCLIです");
+const floatingNumber = program.createOption("-f", "floating number");
+const add = program.createCommand("add").addOption(floatingNumber);
 
 const even = program.createCommand("even").action((str, options) => {
   let n = 0;
   let result = 0;
 
-  while (n < options.args.length) {
-    Number(options.args[n]) % 2 === 0 ? (result = calculate(Number(result), Number(options.args[n]))) : result;
-    n++;
+  if (options.parent.getOptionValue("f")) {
+    // console.log(add.getOptionValue("floatingNumber"));
+    while (n < options.args.length) {
+      Number.parseFloat(options.args[n]) % 2 === 0 ? (result += Number(options.args[n])) : result;
+      n++;
+    }
+    console.log(result);
+  } else {
+    while (n < options.args.length) {
+      Number(options.args[n]) % 2 === 0 ? (result += Number(options.args[n])) : result;
+      n++;
+    }
+    console.log(result);
   }
-  console.log(result);
 });
 
 const odd = program.createCommand("odd").action((str, options) => {
   let n = 0;
   let result = 0;
 
-  while (n < options.args.length) {
-    Number(options.args[n]) % 2 === 1 ? (result = calculate(Number(result), Number(options.args[n]))) : result;
-    n++;
+  if (options.parent.getOptionValue("f")) {
+    while (n < options.args.length) {
+      Number.parseFloat(options.args[n]) % 2 === 1 ? (result += Number(options.args[n])) : result;
+      n++;
+    }
+    console.log(result);
+  } else {
+    while (n < options.args.length) {
+      Number(options.args[n]) % 2 === 1 ? (result += Number(options.args[n])) : result;
+      n++;
+    }
+    console.log(result);
   }
-  console.log(result);
 });
-
-program.name("calculator_cli").description("計算を行うCLIです");
 
 program
   .command("add")
   .description("加算する")
-  .option("-f", "floating number")
+  .arguments("<numbers...>")
   .addCommand(even)
   .addCommand(odd)
-  .action((str, options) => {
+  .addOption(floatingNumber)
+  .action((numbers, option) => {
     let n = 0;
     let result = 0;
 
-    while (n < options.args.length) {
-      if (str.f) {
-        result = calculate(result, Number.parseFloat(options.args[n]));
+    while (n < numbers.length) {
+      if (option.f) {
+        result += Number.parseFloat(numbers[n]);
       } else {
-        Number.isInteger(Number(options.args[n])) ? (result = calculate(result, Number(options.args[n]))) : result;
+        Number.isInteger(Number(numbers[n])) ? (result += Number(numbers[n])) : result;
       }
       n++;
     }
